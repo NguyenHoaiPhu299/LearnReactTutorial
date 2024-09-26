cbcfb0329241bede34d16d644b52e8385247cfda
hoaiphu <rubik1x11@gmail.com>
2024-09-27 02:05:39 +0700

    Example of Class Component (1)

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,14 +1,15 @@
-import React from "react";
+import React, {Component} from "react";
 
-export default class Comment extends React.Component {
+export default class Comment extends Component {
     constructor(props) {
         super(props);
     }
 
     render() {
+        const {title, content} = this.props;
         return(
             <div className="comment">
-                {this.props.title}: {this.props.content}
+                {title}: {content}
             </div>
         )
     }
