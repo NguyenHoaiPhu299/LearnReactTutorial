fb2876d9aec950a2e063e4803446bb6ef6130e74
hoaiphu <rubik1x11@gmail.com>
2024-09-26 19:31:23 +0700

    Ways to use image files

--- src/components/Avartar.js
+++ src/components/Avartar.js
@@ -2,6 +2,9 @@ function Avatar(){
     return(
         <div className="avatar">
             <img src="https://pixlr.com/images/generator/photo-generator.webp" alt="" style={{width: '300px'}}/>
+            <img src="/images/image.jpg" alt="" style={{width: '300px'}}/>
+            {/* <img src="%PUBLIC_URL/images/images" alt="" style={{width: '300px'}}/> */}
+            <img src={process.env.PUBLIC_URL + "/images/image.jpg"} alt="" style={{width: '300px'}}/>
         </div>
     )
 };
