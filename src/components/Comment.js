409348afa43079114c5d9cef91ff13770257b80d
hoaiphu <rubik1x11@gmail.com>
2024-09-27 02:17:04 +0700

    Define in a constructor

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -3,10 +3,9 @@ import React, {Component} from "react";
 export default class Comment extends Component {
     constructor(props) {
         super(props);
-    }
-
-    ShowContent = () => {
-        return <p>Demo Content</p>
+        this.ShowContent = () => {
+            return <p>Demo Content</p>
+        }
     }
 
     render() {
